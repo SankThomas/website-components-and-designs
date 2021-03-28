a18d6d1214b2a33f2ebb3b150f0c8a3911acbe51
Thomas Sankara <thomassankara74@gmail.com>
2021-03-28 10:38:54 +0300

    sample data array

---
+++ grid/grid-app-frontend/src/data/data.js
@@ -0,0 +1,42 @@
+const data = [
+  {
+    id: 1,
+    title: "Price calculator",
+    desc:
+      "How much does 1 Eur in 1963 equal in today's prices? Adjust the rent?",
+    btn: "Calculate price changes",
+  },
+  {
+    id: 2,
+    title: "Name statistics",
+    desc:
+      "How much does 1 Eur in 1963 equal in today's prices? Adjust the rent?",
+    btn: "Search by name",
+  },
+  {
+    id: 3,
+    title: "Stats table",
+    desc: "Find detailed tables with time and create your own",
+    btn: "Go to tables",
+  },
+  {
+    id: 4,
+    title: "Calendar",
+    desc: "See the advance release of calendars in  2021",
+    btn: "Go to calendar",
+  },
+  {
+    id: 5,
+    title: "Subscribe to news",
+    desc: "Get the latest statistics releases and publications by e-mail",
+    btn: "Receive e-mail alerts",
+  },
+  {
+    id: 6,
+    title: "API",
+    desc: "Easily retrive and integrate Statistics with your own system",
+    btn: "Open API",
+  },
+]
+
+export default data
